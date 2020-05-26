1e7935bb68da8afa291df20f7fe91df289e0bd42
ahimneshwar <ahimneshwar@gmail.com>
2020-05-26 18:13:25 +0530

    1st push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [stories, setStories] = useState([])
+
+  useEffect(() => {
+    axios.get('https://thequint-malibu-beta.quintype.io/api/v1/stories')
+      .then(function (r) {
+        console.log(r.data.stories[0]);
+        console.table(r.data.stories);
+        setStories(r.data.stories)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [])
+
+  return (<>
+    {/* <div className="row"> */}
+    <div className="flex-container">
+      {stories.map(story => (
+        <div className="col">
+          <img src={`https://images.assettype.com/${story["hero-image-s3-key"]}`} alt="story" className="image" />
+          <h5>{story.headline}</h5>
+          <p> By <strong> {story["author-name"]}</strong></p>
+        </div>
+      ))
+      }
     </div>
-  );
-}
 
-export default App;
+    {/* </div> */}
+
+  </>)
+}
+export default App
+
